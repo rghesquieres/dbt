ff0d2c491694ac84ebab52901c3817800deb1c56
Raphael Ghesquieres <rghesquieres@gmail.com>
2025-05-13 15:59:22 +0000

    de

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -12,7 +12,7 @@ joined as (
     select
         orders_margin.orders_id,
         orders_margin.date_date,
-        orders_margin.operational_margin + ship.shipping_fee - ship.logCost - ship.ship_cost as operational_margin
+        orders_margin.operational_margin + ship.shipping_fee - ship.`logCost` - ship.ship_cost as operational_margin
     from orders_margin
     left join ship
         on orders_margin.orders_id = ship.orders_id
