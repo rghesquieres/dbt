91eaf47ba0fab13e70c1342fc3929a64044c1c47
Raphael Ghesquieres <rghesquieres@gmail.com>
2025-05-13 14:17:49 +0000

    de

--- models/staging/raw/stg_raw_ship.sql
+++ models/staging/raw/stg_raw_ship.sql
@@ -1,15 +1,14 @@
 with source as (
     select * 
     from {{ source('raw', 'ship') }}
-    where shipping_fee <> shipping_fee_1  -- pour analyser la différence si besoin
 ),
 
 renamed as (
     select
         order_id,
         cast(ship_cost as FLOAT64) as ship_cost,
-        shipping_fee,  -- garde seulement l’un des deux
-        -- supprime ou commente shipping_fee_1 si inutilisé
+        shipping_fee
+        -- on supprime volontairement shipping_fee_1
     from source
 )
 
