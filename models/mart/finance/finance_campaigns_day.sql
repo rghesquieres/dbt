2043af22646ed7c04d4664d4d83a6c86001d3bab
Raphael Ghesquieres <rghesquieres@gmail.com>
2025-05-14 14:19:47 +0000

    x

--- models/mart/finance/finance_campaigns_day.sql
+++ models/mart/finance/finance_campaigns_day.sql
@@ -1,21 +1,20 @@
-SELECT
-  f.date_date,
-  ROUND(f.operational_margin - c.ads_cost, 1) AS ads_margin,
-  f.average_basket,
-  f.operational_margin,
-  c.ads_cost,
-  c.ads_impressions,
-  c.ads_clicks,
-  f.quantity,
-  f.revenue,
-  f.purchase_cost,
-  f.margin,
-  f.shipping_fee,
-  f.logcost,
-  f.ship_cost
+select
+    f.date_date,
+    round(f.operational_margin - c.ads_cost, 1) as ads_margin,
+    f.average_basket,
+    f.operational_margin,
+    c.ads_cost,
+    c.ads_impressions,
+    c.ads_clicks,
+    f.quantity,
+    f.revenue,
+    f.purchase_cost,
+    f.margin,
+    f.shipping_fee,
+    f.logcost,
+    f.ship_cost
 
-FROM {{ ref('finance_days') }} f
-LEFT JOIN {{ ref('int_campaigns_day') }} c
-  ON f.date_date = c.date_date
+from {{ ref("finance_days") }} f
+left join {{ ref("int_campaigns_day") }} c on f.date_date = c.date_date
 
-ORDER BY f.date_date DESC
+order by f.date_date desc
