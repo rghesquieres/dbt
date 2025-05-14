e12860b1741901aa7ed068d29e63cbec24dc9b64
Raphael Ghesquieres <rghesquieres@gmail.com>
2025-05-14 11:13:09 +0000

    m

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,25 +1,14 @@
-with sales as (
-    select *
-    from {{ ref('stg_raw_sales') }}
-),
+ -- int_sales_margin.sql
 
-products as (
-    select *
-    from {{ ref('stg_raw_product') }}
-),
-
-joined as (
-    select
-        sales.orders_id,
-        sales.date_date,
-        sales.revenue,
-        sales.quantity,
-        products.purchase_price,
-        sales.quantity * products.purchase_price as purchase_cost,
-        sales.revenue - (sales.quantity * products.purchase_price) as margin
-    from sales
-    left join products
-        on sales.pdt_id = products.products_id
-)
-
-select * from joined
+  SELECT
+      products_id,
+      date_date,
+      orders_id,
+      revenue,
+      quantity,
+      purchase_price,
+      ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
+      ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin
+  FROM {{ref("stg_raw_sales")}} s
+  LEFT JOIN {{ref("stg_raw_product")}} p
+      USING (products_id)
