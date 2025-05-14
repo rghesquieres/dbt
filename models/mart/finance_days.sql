62bdacfb126206347ab4cba845e4bff2e96e0050
Raphael Ghesquieres <rghesquieres@gmail.com>
2025-05-14 09:46:54 +0000

    s

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -5,7 +5,6 @@
      ,COUNT(orders_id) AS nb_transactions
      ,ROUND(SUM(revenue),0) AS revenue
      ,ROUND(AVG(revenue),1) AS average_basket
-     ,ROUND(SUM(revenue)/COUNT(orders_id),1) AS average_basket_bis
      ,ROUND(SUM(margin),0) AS margin
      ,ROUND(SUM(operational_margin),0) AS operational_margin
      ,ROUND(SUM(purchase_cost),0) AS purchase_cost
