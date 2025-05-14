dbb0c9171082cd3c343525a31b22a3672a74a82f
Raphael Ghesquieres <rghesquieres@gmail.com>
2025-05-14 09:24:17 +0000

    p

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -8,17 +8,28 @@ ship as (
     from {{ ref('stg_raw_ship') }}
 ),
 
+
+sales as (
+    select *
+    from {{ ref('int_sales_margin') }}
+)
+
 joined as (
     select
         orders_margin.orders_id,
         orders_margin.date_date,
+        sum(sales.revenue) as revenue,
         orders_margin.operational_margin
             + ship.shipping_fee
             - ship.`logCost`
             - cast(ship.ship_cost as FLOAT64) as operational_margin
     from orders_margin
+    left join sales
+        on orders_margin.orders_id = sales.orders_id
     left join ship
         on orders_margin.orders_id = ship.orders_id
+    group by orders_margin.orders_id, orders_margin.date_date,
+             orders_margin.operational_margin, ship.shipping_fee, ship.`logCost`, ship.ship_cost
 )
 
 select * from joined
